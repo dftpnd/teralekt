6d3e15877520b6ac1f253a9be165f4d11f1411dc
lkdnvc <lkdnvc@gmail.com>
2016-04-14 01:27:31 +0300

    add logs;

--- script.js
+++ script.js
@@ -2,23 +2,32 @@
 	var TextTransform = function () {
 		this.partSizeElem = document.getElementById('part-size');
 		this.textElem = document.getElementById('text-container');
+		this.logContainer = document.getElementById('log-container');
+	};
+
+
+	TextTransform.prototype.transform = function () {
+		var res = [];
+		var text = this.textElem.value.split('');
+		var textLength = this.textElem.value.length;
+		var partCount = Math.ceil(textLength / this.partSizeElem.value);
+		var part = 1;
+		var separator = '\\';
+
+		while (textLength >= 0) {
+			var row = text.splice(0, this.partSizeElem.value);
+			res.push('[' + part + separator + partCount + '] ' + row.join(''));
+			textLength -= this.partSizeElem.value;
+			++part;
+		}
+		return res;
 	};
 
 	TextTransform.prototype.handle = function (event) {
 		event.preventDefault();
-		var text = this.textElem.value.split('');
-		var textLength = this.textElem.value.length;
-		var res = [];
-		var partCount = Math.ceil(textLength / this.partSizeElem.value);
-		var part = 1;
-		var separator = '\\';
-		while (textLength >= 0) {
-			var row = text.splice(0, this.partSizeElem.value);
-			res.push('[' + part + separator + partCount + ']' + row.join(''));
-			textLength -= this.partSizeElem.value;
-			++part;
-		}
-		console.log(res);
+		var rows = this.transform();
+		this.logContainer.innerHTML = rows.join('<br>');
+
 	};
 
 
